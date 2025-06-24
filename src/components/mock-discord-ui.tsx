b2c2dab9479016ce7f84a0505721508b65ab15be
sohil-khann <khaa40408@gmail.com>
2025-06-24 16:33:32 +0530

    [Sohil Khan] add. dummy msg section in Mock discord ui

--- src/components/mock-discord-ui.tsx
+++ src/components/mock-discord-ui.tsx
@@ -1,9 +1,14 @@
 import { PropsWithChildren } from "react"
 import { Icons } from "./icons"
 import {
+  Inbox,
   PlusCircle,
+  Mic,
+  UserCircle,
+  Headphones,
+  Cog
 } from "lucide-react"
-// import Image from "next/image"
+import Image from "next/image"
 export const MockDiscordUI = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex min-h-[800px] w-full max-w-[1200px] bg-discord-background text-white rounded-lg overflow-hidden shadow-xl">
@@ -30,9 +35,75 @@ export const MockDiscordUI = ({ children }: PropsWithChildren) => {
         <div className="group mt-auto size-12 bg-discord-background rounded-3xl flex items-center justify-center mb-3 hover:rounded-xl transition-all duration-200 hover:bg-[#3ba55c] cursor-not-allowed">
           <PlusCircle className="text-[#3ba55c] group-hover:text-white" />
         </div>
+
       </div>
 
+{/*dm list */}
+  <div className="hidden md:flex w-60 bg-[#2f3136] flex-col">
+        <div className="px-4 h-16 border-b border-[#202225] flex items-center shadow-sm">
+          <div className="w-full bg-[#202225] text-sm rounded px-2 h-8 flex items-center justify-center text-gray-500 cursor-not-allowed">
+            Find or start a conversation
+          </div>
+        </div>
 
+        <div className="flex-1 overflow-y-auto pt-4">
+          <div className="px-2 mb-4">
+            <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddde] cursor-not-allowed">
+              <UserCircle className="mr-4 size-8 text-[#b9bbbe]" />
+              <span className="font-medium text-sm">Friends</span>
+            </div>
+            <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddde] cursor-not-allowed">
+              <Inbox className="mr-4 size-8 text-[#b9bbbe]" />
+              <span className="font-medium text-sm">Nitro</span>
+            </div>
+          </div>
+
+          <div className="px-2 mb-4">
+            <h3 className="text-xs font-semibold text-[#8e9297] px-2 mb-2 uppercase">
+              Direct Messages
+            </h3>
+
+            <div className="flex items-center px-2 py-1.5 rounded bg-[#393c43] text-white cursor-pointer">
+              <Image
+                src="/brand-asset-profile-picture.png"
+                alt="PingPanda Avatar"
+                width={32}
+                height={32}
+                className="object-cover rounded-full mr-3"
+              />
+              <span className="font-medium">Monitrol</span>
+            </div>
+
+            <div className="my-1 space-y-px">
+              {[...Array(4)].map((_, i) => (
+                <div
+                  key={i}
+                  className="flex items-center px-2 py-1.5 rounded text-gray-600 cursor-not-allowed"
+                >
+                  <div className="size-8 rounded-full bg-discord-background mr-3" />
+                  <span className="font-medium">User {i + 1}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="p-2 bg-[#292b2f] flex items-center">
+          <div className="size-8 rounded-full bg-brand-700 mr-2" />
+          <div className="flex-1">
+            <p className="text-sm font-medium text-white">You</p>
+            <p className="text-xs text-[#b9bbbe] flex items-center">
+              @your_account
+            </p>
+          </div>
+
+          <div className="flex items-center space-x-2">
+            <Mic className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer" />
+            <Headphones className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer" />
+            <Cog className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer" />
+          </div>
+        </div>
+      </div>
 </div>
 )
 }
