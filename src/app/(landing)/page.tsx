a1ea0ba5d2397afb1d385d6bab946f94caa0d9b1
sohil-khann <khaa40408@gmail.com>
2025-06-24 15:29:22 +0530

    [Sohil Khan] add. landing page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -52,6 +52,16 @@ const Page = () => {
           </div>
         </MaxWidthWrapper>
       </section>
+        <section className="relative bg-brand-25 pb-4">
+        <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+        <div className="relative mx-auto">
+          <MaxWidthWrapper className="relative">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+            
+            </div>
+          </MaxWidthWrapper>
+        </div>
+      </section>
     </>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ import { ArrowRight } from "lucide-react"
 import { currentUser } from "@clerk/nextjs/server"
 
 export const Navbar = async () => {
-  const user = await currentUser()
+  const user = true
 
   return (
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
