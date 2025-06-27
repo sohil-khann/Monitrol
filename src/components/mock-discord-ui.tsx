4d81249e6eeee57c34668a875d9c95061d10dc7b
sohil-khann <khaa40408@gmail.com>
2025-06-27 20:32:40 +0530

    [Sohil khan ] Add . Mock Discord page UI

--- src/components/mock-discord-ui.tsx
+++ src/components/mock-discord-ui.tsx
@@ -1,12 +1,21 @@
 import { PropsWithChildren } from "react"
 import { Icons } from "./icons"
 import {
-  Inbox,
-  PlusCircle,
-  Mic,
-  UserCircle,
+   Cog,
+  Gift,
   Headphones,
-  Cog
+  HelpCircle,
+  Inbox,
+  Menu,
+  Mic,
+  Phone,
+  Pin,
+  PlusCircle,
+  Search,
+  Smile,
+  Sticker,
+  UserCircle,
+  Video,
 } from "lucide-react"
 import Image from "next/image"
 export const MockDiscordUI = ({ children }: PropsWithChildren) => {
@@ -66,7 +75,7 @@ export const MockDiscordUI = ({ children }: PropsWithChildren) => {
             <div className="flex items-center px-2 py-1.5 rounded bg-[#393c43] text-white cursor-pointer">
               <Image
                 src="/brand-asset-profile-picture.png"
-                alt="PingPanda Avatar"
+                alt="MoniTrol Avatar"
                 width={32}
                 height={32}
                 className="object-cover rounded-full mr-3"
@@ -103,6 +112,56 @@ export const MockDiscordUI = ({ children }: PropsWithChildren) => {
             <Cog className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer" />
           </div>
         </div>
+      </div>
+      {/* Main content */}
+      <div className="flex-1 flex flex-col">
+        {/* dm header */}
+        <div className="h-16 bg-[#36393f]  flex items-center px-4 shadow-md border-b border-[#202225]">
+          <div className="md:hidden mr-4">
+            
+            <Menu className=" size-6 text-[#b9bbbe] hover: text-white cursor-pointer"/>
+
+          </div>
+          <div className="flex items-center">
+            <div className="relative">
+            <Image src="/brand-asset-profile-picture.png" alt="MoniTrol Avatar" width={40} height={40} className="object-cover rounded-full mr-3" />
+ <div className="absolute bottom-0 right-3 size-3 bg-green-500 rounded-full border-2 border-[#36393f]"></div>
+            </div>
+            <p className="font-semibold text-white"> MoniTrol</p>
+            
+          </div>
+
+<div className="ml-auto flex items-center space-x-4 text-[#b9bbbe]">
+  <Phone className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+  <Video className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+  <Pin className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+  <UserCircle className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+  <Search className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+  <Inbox className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+  <HelpCircle className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+</div>
+        </div>
+{/* message history */}
+        <div className="flex-1 overflow-y-auto p-4 bg-discord-background flex flex-col-reverse">{children}</div>
+        {/* messsage input */}
+
+        <div className="p-4">
+          <div className="flex items-center bg-[#40444b] rounded-lg p-1">
+            <PlusCircle className="mx-3 text-[#b9bbbe] hover:text-white cursor-not-allowed" />
+            <input
+              readOnly
+              type="text"
+              placeholder="Message @MoniTrol"
+              className="flex-1 bg-transparent py-2.5 px-1 text-white placeholder-[#72767d] focus:outline-none cursor-not-allowed"
+            />
+            <div className="flex items-center space-x-3 mx-3 text-[#b9bbbe]">
+              <Gift className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+              <Sticker className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+              <Smile className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            </div>
+          </div>
+        </div>
+
       </div>
 </div>
 )
